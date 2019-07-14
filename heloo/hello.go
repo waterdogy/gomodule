4945e906d96eea5c8aec1a83eaebab91f2b872eb
chenyulong <chenyulong.waters@bytedance.com>
2019-07-14 16:14:54 +0800

    test

---
+++ heloo/hello.go
@@ -0,0 +1,11 @@
+package heloo
+
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+
+func Helo(){
+	fmt.Println("helooo!")
+	quote.Go()
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"gomodule/heloo"
+	"rsc.io/quote"
+)
+
+func main(){
+	fmt.Println(quote.Hello())
+	heloo.Helo()
+}
+
